b24c0556bcf5c295d3caaad2a04e7124aaf8a48c
taniguchi-yuta <>
2023-10-09 23:11:21 +0900

    Vuestic Adminがgitにアップされていなかったから、再度pushし直した。ユーザー登録とログインについてrouter関連は作成中。

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,7 @@
+<!-- src/views/Login.vue -->
+<template>
+  <div>
+    <h1>Login</h1>
+    <!-- ログインフォームの内容 -->
+  </div>
+</template>
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,7 @@
+<!-- src/views/Register.vue -->
+<template>
+  <div>
+    <h1>Register</h1>
+    <!-- 登録フォームの内容 -->
+  </div>
+</template>
