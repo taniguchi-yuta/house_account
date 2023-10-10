5fe64238c4e43d35bbfc183db99302b73d93b013
taniguchi-yuta <>
2023-10-10 23:00:48 +0900

    ユーザー登録・ログイン画面のデザインたたき台

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,7 +1,36 @@
-<!-- src/views/Login.vue -->
 <template>
-  <div>
-    <h1>Login</h1>
-    <!-- ログインフォームの内容 -->
+  <div class="login-container">
+    <va-card flat>
+      <va-form>
+        <va-input label="メールアドレス" v-model="email" flat/>
+        <va-input type="password" label="パスワード" v-model="password" flat/>
+        <va-button @click="login" flat>ログイン</va-button>
+      </va-form>
+    </va-card>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      // ログインロジックをここに追加
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,7 +1,38 @@
-<!-- src/views/Register.vue -->
 <template>
-  <div>
-    <h1>Register</h1>
-    <!-- 登録フォームの内容 -->
+  <div class="registration-container">
+    <va-card flat>
+      <va-form>
+        <va-input label="メールアドレス" v-model="email" flat/>
+        <va-input type="password" label="パスワード" v-model="password" flat/>
+        <va-input label="ユーザー名" v-model="username" flat/>
+        <va-button @click="register" flat>登録</va-button>
+      </va-form>
+    </va-card>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      username: ''
+    };
+  },
+  methods: {
+    register() {
+      // 登録ロジックをここに追加
+    }
+  }
+};
+</script>
+
+<style scoped>
+.registration-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
